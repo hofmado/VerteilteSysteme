67f5c80ee67e9e6eab0f46a023c099bfd550ba29
Dominik Hoffmann <dominik.r.hoffmann@googlemail.com>
2023-03-20 12:09:33 +0000

    bug fix in KalkPage

--- Beispielanwendung Adressbuch/Frontend/src/page-kalk/KalkPage.js
+++ Beispielanwendung Adressbuch/Frontend/src/page-kalk/KalkPage.js
@@ -17,36 +17,28 @@ export default class KalkPage extends Page {
         
         const getDataButton = this._mainElement.querySelector('#get-data');
         const saveDataButton = this._mainElement.querySelector('#save-data');
-        const input1 = this._mainElement.querySelector('#input1');
-        const input2 = this._mainElement.querySelector('#input2');
-        const input3 = this._mainElement.querySelector('#input3');
-        const input4 = this._mainElement.querySelector('#input4');
-        const output1 = this._mainElement.querySelector('#output1');
-        const output2 = this._mainElement.querySelector('#output2');
-        const output3 = this._mainElement.querySelector('#output3');
-        const output4 = this._mainElement.querySelector('#output4');
+        const feldJahr = this._mainElement.querySelector('#feldJahr');
+        const feldWerbungskosten = this._mainElement.querySelector('#FeldWerbungskosten');
+        const feldFahrtweg = this._mainElement.querySelector('#feldFahrtweg');
+        const feldSteuerablassung = this._mainElement.querySelector('#feldSteuerablassung');
 
         const form = document.querySelector("form");
 
-        
+        //Buttonmethode für die GET-Anfrage
         getDataButton.addEventListener('click', () => {
-        fetch('/get-data-url')
+        fetch('/steuerjahr/')
             .then(response => response.json())
             .then(data => {
-                input1.value = data.value1;
-                input2.value = data.value2;
-                input3.value = data.value3;
-                input4.value = data.value4;
+                feldSteuerablassung.value = data.steuerablassung;
             });
         });
         
-        
+        //Buttonmethode für die PUT-Anfrage
         saveDataButton.addEventListener('click', () => {
             const data = {
-                value1: input1.value,
-                value2: input2.value,
-                value3: input3.value,
-                value4: input4.value,
+                jahr: feldJahr.jahr,
+                werbungskosten: feldWerbungskosten.value,
+                fahrtweg: feldFahrtweg.value,
             };
 
             fetch('/save-data-url', {
@@ -58,10 +50,7 @@ export default class KalkPage extends Page {
             })
             .then(response => response.json())
             .then(data => {
-                output1.innerText = data.value1;
-                output2.innerText = data.value2;
-                output3.innerText = data.value3;
-                output4.innerText = data.value4;
+                steuerablassung.innerText = data.steuerablassung;
             });
         });
           
