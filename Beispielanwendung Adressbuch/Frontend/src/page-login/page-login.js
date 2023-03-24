f240047341e35ee3781579774424216eab4f30b8
leomax22 <leomax22@web.de>
2023-03-24 23:03:27 +0000

    Login frontend Logik fast finalisiert

--- Beispielanwendung Adressbuch/Frontend/src/page-login/page-login.js
+++ Beispielanwendung Adressbuch/Frontend/src/page-login/page-login.js
@@ -10,6 +10,9 @@ import HtmlTemplate from "./page-login.html";
 export default class PageLogin extends Page {
     constructor(app, editId) {
         super(app, HtmlTemplate);
+
+        this.username = null;
+        this.password = null;
         
     }
     async init() {
@@ -17,19 +20,63 @@ export default class PageLogin extends Page {
         await super.init();
         this._url = '/login'
         this._title = "Login";
+        // Logindaten laden 
+        this._url = `/address/${this._editId}`;
+        this._dataset = await this._app.backend.fetch("GET", this._url);
+
         
-        this.username = null;
-        this.password = null;
-        this.submitbutton = document.getElementById("submit");
+        let loginbutton = document.getElementById("login");
+        let submitbutton = document.getElementById("submit");
             // Event Handler registrieren
-            this.submitbutton.addEventListener("click", () => this._askLogin(dataset._id));
+        loginbutton.addEventListener("click", () => this._askLogin());
+        submitbutton.addEventListener("click", () => this._register());
         }
-        async _askLogin(id) {
-            fetch('/login',{
-                method: 'POST',
-                headers
-            })
+        async _askLogin() {
+            this._dataset._id        = this._editId;
+            this._dataset.username = this._firstNameInput.value.trim();
+            this._dataset.password  = this._lastNameInput.value.trim();
+    
+            if (!this._dataset.username) {
+                alert("Geben Sie erst einen Usernamen ein.");
+                return;
+            }
+    
+            if (!this._dataset.password) {
+                alert("Geben Sie erst einen Passwort ein.");
+                return;
+            }
+              
         }
     }
+    async _register() {
+        // Eingegebene Werte prüfen
+        this._dataset._id        = this._editId;
+        this._dataset.username = this._firstNameInput.value.trim();
+        this._dataset.password  = this._lastNameInput.value.trim();
+
+        if (!this._dataset.username) {
+            alert("Geben Sie erst einen Usernamen ein.");
+            return;
+        }
+
+        if (!this._dataset.password) {
+            alert("Geben Sie erst einen Passwort ein.");
+            return;
+        }
+
+        // Datensatz speichern
+        try {
+                await this._app.backend.fetch("POST", this._url, {body: this._dataset});
+
+        } catch (ex) {
+            this._app.showException(ex);
+            return;
+        }
+
+        // weiterleitung zum Datasheet
+        location.hash = "#/";
+    }
+};
+
 
     
