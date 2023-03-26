d83215e29292d75894647c312477258c8f09ba3c
F98R R <felix.rebmann@web.de>
2023-03-26 21:09:13 +0000

    .

--- Beispielanwendung Adressbuch/Backend/src/database.js
+++ Beispielanwendung Adressbuch/Backend/src/database.js
@@ -1,98 +1,52 @@
-"use strict"
+import DatabaseFactory from "../database.js";
+import {CURSOR_FLAGS, ObjectId} from "mongodb";
 
-import { MongoClient } from "mongodb";
+export default class steuerjahr_service {
 
+  constructor() {
+    this._steuerjahr = DatabaseFactory.database.collection("Steuerjahr");
+  }
 /**
- * Singleton-Klasse zum Zugriff auf das MongoDB-Datenbankobjekt, ohne dieses
- * ständig als Methodenparameter durchreichen zu müssen. Stattdessen kann
- * einfach das Singleton-Objekt dieser Klasse importiert und das Attribut
- * `mongodb` oder `database` ausgelesen werden.
- */
-class DatabaseFactory {
-    /**
-     * Ersatz für den Konstruktor, damit aus dem Hauptprogramm heraus die
-     * Verbindungs-URL der MongoDB übergeben werden kann. Hier wird dann
-     * auch gleich die Verbindung hergestellt.
-     *
-     * @param {String} connectionUrl URL-String mit den Verbindungsdaten
-     */
-    async init(connectionUrl) {
-        // Datenbankverbindung herstellen
-        this.client = new MongoClient(connectionUrl);
-        await this.client.connect();
-        this.database = this.client.db("Steuerjahr");
+   * Speichern eines neuen Steuerjahrs.
+   *
+   * @param {Object} jahr Zu gespeichertem Steuerjahr
+   * @return {Promise} zu gespeichertes Steuerjahr
+   */
+  async read(query) {
+    let cursor = await this._steuerjahr.findOne(query, {
+      sort: {
+        jahr:1,
+      }
+    });
+    return cursor.toArray();
+  }
 
-        await this._createDemoData();
-    }
+  /**
+   * Speichern eines neuen Steuerjahrs.
+   *
+   * @param {Object} user Zu speichernder Steuerjahr an User
+   * @return {Promise} zu speicherndes Stuerjahr an User
+   */
+  async create(user) {
+      if(user == null) return;
 
-    /**
-     * Hilfsmethode zum Anlegen von Demodaten. Würde man so in einer
-     * Produktivanwendung natürlich nicht machen, aber so sehen wir
-     * wenigstens gleich ein paar Daten.
-     */
-    async _createDemoData() {
-        //// TODO: Methode anpassen, um zur eigenen App passende Demodaten anzulegen ////
-        //// oder die Methode ggf. einfach löschen und ihren Aufruf oben entfernen.  ////
-        /*let user = this.database.collection("user");
-        let steuerjahr = this.database.collection("steuerjahr");*/
+      // Get input values
+      const kosten = parseInt(document.getElementById("kosten").value);
+      const fahrtweg = parseInt(document.getElementById("fahrtweg").value);
 
-        if (await user.estimatedDocumentCount() === 0) {
-            user.insertMany([
-                {
-                    user_name: "Max",
-                    password: "1",
-                    gesamt_steuer: "6000",
-                    semester_anzahl: "4",
-                    steuerjahr: {
-                        jahr: "2022",
-                        steuerablassungen: "2500",
-                    },
-                    steuerjahr: {
-                        jahr: "2021",
-                        steuerablassungen: "3500",
-                    },
-                },
-            ]);
-        }     
-    }
+      // Calculate tax savings
+      const fahrtkosten = fahrtweg * 0.3;
+      const newWerbungskosten = fahrtkosten + kosten ;
+    
+      let newSteuerJahr = {
+        jahr:               user.steuerjahr.jahr           || parseInt(document.getElementById("jahr").value),
+        werbungskosten:     user.steuerjahr.werbungskosten || newWerbungskosten,
+      };
 
-    //Methode um Steuerjahre bzw. Semester zusammenzuzählen und zu übergeben
-    async _getSteuerjahre() {
-        let user = this.database.collection("user");
-        let steuerjahr = this.database.collection("steuerjahr");
-        let gesamtersparnis = 0;
-
-        if (await user.semester_anzahl === 0) {
-            //Tu nichts, da keine Semester eingetragen wurden
-            ;         
-        }
-        
-        else {
-            //Daten holen
-            user.forEach(user.steuerjahr() {
-               gesamtersparnis += steuerjahr.steuerablassungen; //Was ist hier falsch?
-            });
-
-            //Gesamtersparnis in DB festhalten
-            user.insert() //Hier Gesamtersparnis füllen
-            return gesamtersparnis;
-
-        }
-    }
-
-    //Methode um zu prüfen ob Nebenjob stattgefunden hat
-    async _getNebenjob() {
-        if (await user.nebenjob === false) {
-            //Tu nichts, da kein Nebenjob eingetragen wurden
-            ;         
-        }
-        
-        else {
-            //Daten holen
-            return user._getNebenjob();
-        }
-    }
-
+    // Display tax savings
+      document.getElementById("werbungskosten").innerHTML = absetzbarerbetrag.toFixed(2);
+      document.getElementById("jahr").innerHTML = jahr.toFixed(0);
+        let result = await this._steuerjahr.insertOne(newSteuerJahr);
+        return await this._steuerjahr.findOne({_id: result.insertedId});
+  }
 }
-
-export default new DatabaseFactory();
--- Beispielanwendung Adressbuch/Backend/src/service/berechnungen.js
+++ Beispielanwendung Adressbuch/Backend/src/service/berechnungen.js
@@ -1,113 +1,52 @@
-//Art des Studiums
+import DatabaseFactory from "../database.js";
+import {CURSOR_FLAGS, ObjectId} from "mongodb";
 
-//Anzahl der Semester
+export default class service { /*angepasst*/ 
 
-//Nebenbeschäftigung
-//HiWi
-//2. Job
-//Aushilfsjob
+  constructor() {
+    this._berechnungen = DatabaseFactory.database.collection("Steuerjahr");
+  }
+/**
+   * Speichern eines neuen Steuerjahrs.
+   *
+   * @param {Object} jahr Zu gespeichertem Steuerjahr
+   * @return {Promise} zu gespeichertes Steuerjahr
+   */
+  async read(query) {
+    let cursor = await this._steuerjahr.findOne(query, {
+      sort: {
+        jahr:1,
+      }
+    });
+    return cursor.toArray();
+  }
 
-/* Abgabepflicht oder freiwillige Steuererklärung?
-– freiwillig: z. B. bei Anstellung als Werkstudent:in oder Jobben in den Semesterferien
-– Abgabepflicht: z. B. bei mehreren Jobs parallel neben dem Studium
-Studienkosten absetzbar als Sonderausgaben oder Werbungskosten
-– Erststudium: jährlich 6.000 Euro als Sonderausgaben, kein Verlustvortrag möglich
-– Zweitstudium: Kosten in unbegrenzter Höhe als Werbungskosten absetzbar und Verlustvortrag möglich
-Definition Zweitstudium = Studium nach einer abgeschlossenen Erstausbildung bzw. einem abgeschlossenen Erststudium
-Duales Studium: Studienkosten auch als Werbungskosten absetzbar und Verlustvortrag möglich
-Abgabefristen der Steuererklärung
-– bei Abgabepflicht: Abgabefrist ohne steuerlichen Vertreter am 31.07. des Folgejahres, mit steuerlichem Vertreter am letzten Februartag des Zweitfolgejahres
-– bei keiner Abgabepflicht: Abgabefrist am 31.12. des 4. Folgejahres bzw. Verlustvortrag bis zu 7 Jahre rückwirkend möglich*/
+  /**
+   * Speichern eines neuen Steuerjahrs.
+   *
+   * @param {Object} user Zu speichernder Steuerjahr an User
+   * @return {Promise} zu speicherndes Stuerjahr an User
+   */
+  async create(user) {
+      if(user == null) return;
 
-//Rückwirkende Steuererklärung, 7 Jahre für Studenten
+      // Get input values
+      const kosten = parseInt(document.getElementById("kosten").value);
+      const fahrtweg = parseInt(document.getElementById("fahrtweg").value);
 
-// Import der Klasse zum Zugriff auf die MongoDB
-import { MongoClient } from "mongodb";
+      // Calculate tax savings
+      const fahrtkosten = fahrtweg * 0.3;
+      const newWerbungskosten = fahrtkosten + kosten ;
+    
+      let newSteuerJahr = {
+        jahr:               user.steuerjahr.jahr           || parseInt(document.getElementById("jahr").value),
+        werbungskosten:     user.steuerjahr.werbungskosten || newWerbungskosten,
+      };
 
-// Verbindung mit dem Datenbankserver herstellen
-let client = new MongoClient("mongodb://localhost:27017");
-await this.client.connect();
-
-// Öffnen einer Datenbank (Sammelcontainer für mehrere Collections)
-let database = this.client.db("adressbook");
-
-// Zugriff auf eine einzelne Collection ermöglichen (beinhaltet die Daten)
-let addresses = this.database.collection("addresses");
-
-// Anzahl der vorhandenen Einträge schätzen
-let count = await addresses.estimatedDocumentCount();
-
-// Neue Einträge anlegen
-addresses.insertMany([
-    {
-        first_name: "Willy",
-        last_name: "Tanner",
-        phone: "+49 711 564412",
-        email: "willy.tanner@alf.com",
-    },
-    {
-        first_name: "Michael",
-        last_name: "Knight",
-        phone: "+49 721 554194",
-        email: "michael@knight-rider.com",
-    },
-]);
-
-// Auslesen eines Eintrags anhand seiner ID
-let id = "47110815";
-let result = await addresses.findOne({_id: new ObjectId(id)});
-
-// Löschen eines Eintrags anhand seiner ID
-let result = this._addresses.deleteOne({_id: new ObjectId(id)});
-let count = result.deletedCount;
-
-// Speichern eines neuen Eintrags
-let newAddress = {
-    first_name: address.first_name || "",
-    last_name:  address.last_name  || "",
-    phone:      address.phone      || "",
-    email:      address.email      || "",
-};
-
-let result = await this._addresses.insertOne(newAddress);
-
-// Einträge suchen und zurückgeben
-let query = {
-    first_name: "Willy",
-};
-
-let cursor = addresses.find(query, {
-    sort: {
-        first_name: 1,
-        last_name: 1,
-    }
-});
-
-let result = cursor.toArray();
-
-// Ändern eines Eintrags
-let updateDoc = {
-    $set: {
-        first_name: "Willy",
-        last_name: "Tanner",
-    },
+    // Display tax savings
+      document.getElementById("werbungskosten").innerHTML = absetzbarerbetrag.toFixed(2);
+      document.getElementById("jahr").innerHTML = jahr.toFixed(0);
+        let result = await this._steuerjahr.insertOne(newSteuerJahr);
+        return await this._steuerjahr.findOne({_id: result.insertedId});
+  }
 }
-
-//Berechnungen vornehmen
-let semester = [];
-
-let anzahlSemester = semester.length();
-
-let gesamtersparnis = 0;
-
-//Steuereinsparungen über alle Semester addieren
-let steuernges;
-
-for(let i = 0; i < semester.length; i++){
-
-    gesamtersparnis += semester[i];
-}
-
-
-await addresses.updateOne({_id: new ObjectId(id)}, updateDoc);
-let newEntry = addresses.findOne({_id: new ObjectId(id)});
